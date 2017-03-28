fbc80ce7a4f99001bc18d7229bc8fdd49bbe7ea5
Juan Medina <jamedina@gmail.com>
2017-03-28 16:06:52 +0100

    make it more functional

--- src/test/java/com/juan/medina/microservices/ReactiveMsExampleApplicationTests.java
+++ src/test/java/com/juan/medina/microservices/ReactiveMsExampleApplicationTests.java
@@ -7,12 +7,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ReactiveHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.FluxExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.reactive.server.WebTestClient.UriSpec;
+import org.springframework.web.reactive.function.BodyInserter;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.util.UriBuilder;
 import reactor.test.StepVerifier;
+
+import java.net.URI;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -24,6 +32,7 @@ public class ReactiveMsExampleApplicationTests {
 
     private static final String DEFAULT_VALUE = "world";
     private static final String CUSTOM_VALUE = "reactive";
+    private static final String JSON_VALUE = "json";
     private static final String HELLO_PATH = "/hello";
     private static final String NAME_ARG = "{name}";
 
@@ -37,66 +46,65 @@ public class ReactiveMsExampleApplicationTests {
         client = WebTestClient.bindToRouterFunction(helloRouterFunction).build();
     }
 
-    @Test
-    public void defaultHelloTest() {
-
-        FluxExchangeResult<HelloResponse> result = client.get()
-                .uri(HELLO_PATH)
+    private FluxExchangeResult<HelloResponse> invoke(UriSpec spec, Function<UriBuilder, URI> url,
+                                                     BodyInserter<HelloRequest, ReactiveHttpOutputMessage> object) {
+        return spec.uri(url)
                 .accept(APPLICATION_JSON_UTF8)
-                .exchange()
+                .exchange(object)
                 .expectStatus().isOk()
                 .expectHeader().contentType(APPLICATION_JSON_UTF8)
                 .expectBody(HelloResponse.class)
                 .returnResult();
+    }
 
+    private FluxExchangeResult<HelloResponse> invoke(UriSpec spec, Function<UriBuilder, URI> url) {
+        return invoke(spec, url, null);
+    }
+
+    private void verify(FluxExchangeResult<HelloResponse> result, Consumer<HelloResponse> check) {
         StepVerifier.create(result.getResponseBody())
-                .consumeNextWith(it ->
-                        assertThat(it.getHello(), is(DEFAULT_VALUE)
-                        ))
+                .consumeNextWith(check)
                 .expectComplete()
                 .verify();
     }
 
     @Test
+    public void defaultHelloTest() {
+
+        verify(
+                invoke(
+                        client.get(),
+                        builder -> builder.path(HELLO_PATH).build()),
+                it -> {
+                    assertThat(it.getHello(), is(DEFAULT_VALUE));
+                }
+        );
+    }
+
+    @Test
     public void getHelloTest() {
-
-        FluxExchangeResult<HelloResponse> result = client.get()
-                .uri(builder -> builder.path(HELLO_PATH)
-                .path("/").path(NAME_ARG).build(CUSTOM_VALUE))
-                .accept(APPLICATION_JSON_UTF8)
-                .exchange()
-                .expectStatus().isOk()
-                .expectHeader().contentType(APPLICATION_JSON_UTF8)
-                .expectBody(HelloResponse.class)
-                .returnResult();
-
-        StepVerifier.create(result.getResponseBody())
-                .consumeNextWith(it ->
-                        assertThat(it.getHello(), is(CUSTOM_VALUE)
-                        ))
-                .expectComplete()
-                .verify();
+        verify(
+                invoke(
+                        client.get(),
+                        builder -> builder.path(HELLO_PATH).path("/").path(NAME_ARG).build(CUSTOM_VALUE)),
+                it -> {
+                    assertThat(it.getHello(), is(CUSTOM_VALUE));
+                }
+        );
     }
 
     @Test
     public void postHelloTest() {
 
-        FluxExchangeResult<HelloResponse> result = client.post()
-                .uri(HELLO_PATH)
-                .accept(APPLICATION_JSON_UTF8)
-                .exchange(BodyInserters.fromObject(new HelloRequest(CUSTOM_VALUE)))
-                .expectStatus().isOk()
-                .expectHeader().contentType(APPLICATION_JSON_UTF8)
-                .expectBody(HelloResponse.class)
-                .returnResult();
-
-        StepVerifier.create(result.getResponseBody())
-                .consumeNextWith(it ->
-                        assertThat(it.getHello(), is(CUSTOM_VALUE)
-                        ))
-                .expectComplete()
-                .verify();
-
+        verify(
+                invoke(
+                        client.post(),
+                        builder -> builder.path(HELLO_PATH).build(),
+                        BodyInserters.fromObject(new HelloRequest(JSON_VALUE))),
+                it -> {
+                    assertThat(it.getHello(), is(JSON_VALUE));
+                }
+        );
     }
 
 }
