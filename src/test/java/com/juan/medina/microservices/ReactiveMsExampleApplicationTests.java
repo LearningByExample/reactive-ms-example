f021acbc7659fe82e0fb91f9c3e165353a8e2350
Juan Medina <jamedina@gmail.com>
2017-04-22 10:57:03 +0100

    simplify changes

--- src/test/java/com/juan/medina/microservices/ReactiveMsExampleApplicationTests.java
+++ src/test/java/com/juan/medina/microservices/ReactiveMsExampleApplicationTests.java
@@ -7,24 +7,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ReactiveHttpOutputMessage;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.test.web.reactive.server.WebTestClient.RequestBodySpec;
-import org.springframework.test.web.reactive.server.WebTestClient.UriSpec;
-import org.springframework.web.reactive.function.BodyInserter;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.util.UriBuilder;
 
 import java.net.URI;
-import java.util.function.Consumer;
 import java.util.function.Function;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
+import static org.springframework.test.web.reactive.server.WebTestClient.bindToRouterFunction;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -43,68 +38,60 @@ public class ReactiveMsExampleApplicationTests {
 
     @Before
     public void setup() {
-        client = WebTestClient.bindToRouterFunction(helloRouterFunction).build();
+        client = bindToRouterFunction(helloRouterFunction).build();
     }
 
-    private EntityExchangeResult<HelloResponse> completeInvoke(WebTestClient.RequestHeadersSpec<?> spec) {
-        return spec.accept(APPLICATION_JSON_UTF8).exchange()
+    public <T> T get(Function<UriBuilder, URI> builder, Class<T> type){
+
+        return client.get()
+                .uri(builder)
+                .accept(APPLICATION_JSON_UTF8).exchange()
                 .expectStatus().isOk()
                 .expectHeader().contentType(APPLICATION_JSON_UTF8)
-                .expectBody(HelloResponse.class)
-                .returnResult();
+                .expectBody(type)
+                .returnResult().getResponseBody();
     }
 
-    private EntityExchangeResult<HelloResponse> invoke(UriSpec<RequestBodySpec> spec, Function<UriBuilder, URI> url,
-                                                     BodyInserter<HelloRequest, ReactiveHttpOutputMessage> object) {
-        return completeInvoke(spec.uri(url).body(object));
+    public <T,K> T post(Function<UriBuilder, URI> builder, K object,Class<T> type){
+
+        return client.post()
+                .uri(builder)
+                .body(BodyInserters.fromObject(object))
+                .accept(APPLICATION_JSON_UTF8).exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(APPLICATION_JSON_UTF8)
+                .expectBody(type)
+                .returnResult().getResponseBody();
     }
-
-    private EntityExchangeResult<HelloResponse> invoke(UriSpec spec, Function<UriBuilder, URI> url) {
-
-        return completeInvoke(spec.uri(url));
-    }
-
-    private void verify(EntityExchangeResult<HelloResponse> result, Consumer<HelloResponse> check) {
-        check.accept(result.getResponseBody());
-    }
-
 
     @Test
     public void defaultHelloTest() {
-        verify(
-                invoke(
-                        client.get(),
-                        builder -> builder.path(HELLO_PATH).build()),
-                it -> {
-                    assertThat(it.getHello(), is(DEFAULT_VALUE));
-                }
-        );
+
+        HelloResponse response = get(
+                builder -> builder.path(HELLO_PATH).build(),
+                HelloResponse.class);
+
+        assertThat(response.getHello(), is(DEFAULT_VALUE));
     }
 
     @Test
     public void getHelloTest() {
-        verify(
-                invoke(
-                        client.get(),
-                        builder -> builder.path(HELLO_PATH).path("/").path(NAME_ARG).build(CUSTOM_VALUE)),
-                it -> {
-                    assertThat(it.getHello(), is(CUSTOM_VALUE));
-                }
-        );
+
+        HelloResponse response = get(
+                builder -> builder.path(HELLO_PATH).path("/").path(NAME_ARG).build(CUSTOM_VALUE),
+                HelloResponse.class);
+
+        assertThat(response.getHello(), is(CUSTOM_VALUE));
     }
 
     @Test
     public void postHelloTest() {
 
-        verify(
-                invoke(
-                        client.post(),
-                        builder -> builder.path(HELLO_PATH).build(),
-                        BodyInserters.fromObject(new HelloRequest(JSON_VALUE))),
-                it -> {
-                    assertThat(it.getHello(), is(JSON_VALUE));
-                }
-        );
-    }
+        HelloResponse response = post(
+                builder -> builder.path(HELLO_PATH).build(),
+                new HelloRequest(JSON_VALUE),
+                HelloResponse.class);
 
+        assertThat(response.getHello(), is(JSON_VALUE));
+    }
 }
