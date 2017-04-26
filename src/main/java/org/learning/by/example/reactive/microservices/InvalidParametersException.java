77d7e970d02529c1d02c175a2f99fcb2917d2ad9
Juan Medina <jamedina@gmail.com>
2017-04-27 00:16:43 +0100

    more functional error handling

--- src/main/java/org/learning/by/example/reactive/microservices/InvalidParametersException.java
+++ src/main/java/org/learning/by/example/reactive/microservices/InvalidParametersException.java
@@ -1,7 +1,6 @@
 package org.learning.by.example.reactive.microservices;
 
 class InvalidParametersException extends Exception{
-
     InvalidParametersException(String message){
         super(message);
     }
