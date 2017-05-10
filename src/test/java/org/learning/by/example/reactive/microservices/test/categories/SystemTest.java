8686e0ad31dde80b72f220b358a37476cc4b1e6d
Juan Medina <jamedina@gmail.com>
2017-05-10 23:01:11 +0100

    configure profiles

--- src/test/java/org/learning/by/example/reactive/microservices/test/categories/SystemTest.java
+++ src/test/java/org/learning/by/example/reactive/microservices/test/categories/SystemTest.java
@@ -2,6 +2,13 @@ package org.learning.by.example.reactive.microservices.test.categories;
 
 import org.junit.jupiter.api.Tag;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ ElementType.TYPE, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
 @Tag("SystemTest")
 public @interface SystemTest {
 }
