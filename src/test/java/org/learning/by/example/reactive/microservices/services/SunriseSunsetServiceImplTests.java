efa73567f56625240436d222b4b9680324db9d85
Juan Medina <jamedina@gmail.com>
2017-05-19 08:08:32 +0100

    start mocks

--- src/test/java/org/learning/by/example/reactive/microservices/services/SunriseSunsetServiceImplTests.java
+++ src/test/java/org/learning/by/example/reactive/microservices/services/SunriseSunsetServiceImplTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.learning.by.example.reactive.microservices.model.Location;
 import org.learning.by.example.reactive.microservices.model.SunriseSunset;
+import org.learning.by.example.reactive.microservices.model.SunriseSunsetResult;
 import org.learning.by.example.reactive.microservices.test.categories.UnitTest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.mock.mockito.SpyBean;
@@ -14,6 +15,9 @@ import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.text.IsEmptyString.isEmptyOrNullString;
+import static org.learning.by.example.reactive.microservices.test.RestServiceHelper.getMonoFromJsonPath;
+import static org.learning.by.example.reactive.microservices.test.RestServiceHelper.mockWebClient;
+import static org.mockito.Mockito.reset;
 
 @UnitTest
 @DisplayName("SunriseSunsetServiceImplTests Unit Tests")
@@ -25,6 +29,10 @@ class SunriseSunsetServiceImplTests {
     private static final Mono<Location> GOOGLE_LOCATION_MONO = Mono.just(GOOGLE_LOCATION);
     private static final String GOOGLE_LOCATION_IN_PARAMS = "?lat="+Double.toString(GOOGLE_LAT)+"&lng="+Double.toString(GOOGLE_LNG);
 
+    private static final String JSON_OK = "/json/SunriseSunsetResult_OK.json";
+    private static final Mono<SunriseSunsetResult> SUNRISE_SUNSET_OK = getMonoFromJsonPath(JSON_OK, SunriseSunsetResult.class);
+    private static final String OK = "OK";
+
     @Value("${SunriseSunsetServiceImpl.endPoint}")
     private
     String endPoint;
@@ -35,6 +43,19 @@ class SunriseSunsetServiceImplTests {
     @Test
     void getBeamTest() {
         assertThat(sunriseSunsetService, is(notNullValue()));
+    }
+
+    @Test
+    void getMockingWebClientTest() {
+        sunriseSunsetService.webClient = mockWebClient(sunriseSunsetService.webClient, SUNRISE_SUNSET_OK);
+
+        SunriseSunsetResult result = Mono.just(endPoint.concat(GOOGLE_LOCATION_IN_PARAMS)).transform(sunriseSunsetService::get).block();
+
+        assertThat(result, is(notNullValue()));
+        assertThat(result.getResults(), is(notNullValue()));
+        assertThat(result.getStatus(), is(OK));
+
+        reset(sunriseSunsetService.webClient);
     }
 
     @Test
