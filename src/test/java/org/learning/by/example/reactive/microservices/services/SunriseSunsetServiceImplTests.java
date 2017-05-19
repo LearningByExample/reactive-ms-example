ccdd8b1223839e288aa65bbd891ff3ebd75a79c1
Juan Medina <jamedina@gmail.com>
2017-05-19 21:28:04 +0100

    add one test for runtime exceptions

--- src/test/java/org/learning/by/example/reactive/microservices/services/SunriseSunsetServiceImplTests.java
+++ src/test/java/org/learning/by/example/reactive/microservices/services/SunriseSunsetServiceImplTests.java
@@ -42,6 +42,7 @@ class SunriseSunsetServiceImplTests {
     private static final Mono<SunriseSunsetResult> SUNRISE_SUNSET_KO = getMonoFromJsonPath(JSON_KO, SunriseSunsetResult.class);
     private static final Mono<SunriseSunsetResult> SUNRISE_SUNSET_EMPTY = getMonoFromJsonPath(JSON_EMPTY, SunriseSunsetResult.class);
     private static final Mono<SunriseSunsetResult> LOCATION_EXCEPTION = Mono.error(new GetSunriseSunsetException(BAD_EXCEPTION));
+    private static final Mono<SunriseSunsetResult> BIG_EXCEPTION = Mono.error(new RuntimeException(BAD_EXCEPTION));
 
 
     @Value("${SunriseSunsetServiceImpl.endPoint}")
@@ -131,6 +132,28 @@ class SunriseSunsetServiceImplTests {
 
         reset(sunriseSunsetService);
     }
+
+    @Test
+    void fromLocationBigExceptionTest() {
+
+        doReturn(BIG_EXCEPTION).when(sunriseSunsetService).get(any());
+
+        SunriseSunset result = GOOGLE_LOCATION_MONO.transform(sunriseSunsetService::fromLocation)
+                .onErrorResume(throwable -> {
+                    assertThat(throwable, instanceOf(GetSunriseSunsetException.class));
+                    return Mono.empty();
+                }).block();
+
+        assertThat(result, is(nullValue()));
+
+        verify(sunriseSunsetService, times(1)).fromLocation(any());
+        verify(sunriseSunsetService, times(1)).buildUrl(any());
+        verify(sunriseSunsetService, times(1)).get(any());
+        verify(sunriseSunsetService, times(1)).createResult(any());
+
+        reset(sunriseSunsetService);
+    }
+
 
     @Test
     void fromLocationErrorTest() {
