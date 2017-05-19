dec5599df52b443a1ddb7d557b09c09b532d0f06
Juan Medina <jamedina@gmail.com>
2017-05-19 21:23:31 +0100

    complete mocking of the service

---
+++ src/main/java/org/learning/by/example/reactive/microservices/exceptions/GetSunriseSunsetException.java
@@ -0,0 +1,11 @@
+package org.learning.by.example.reactive.microservices.exceptions;
+
+public class GetSunriseSunsetException extends Exception{
+    public GetSunriseSunsetException(final String message, final Throwable throwable) {
+        super(message, throwable);
+    }
+
+    public GetSunriseSunsetException(final String message) {
+        super(message);
+    }
+}
--- src/main/java/org/learning/by/example/reactive/microservices/services/SunriseSunsetServiceImpl.java
+++ src/main/java/org/learning/by/example/reactive/microservices/services/SunriseSunsetServiceImpl.java
@@ -1,6 +1,6 @@
 package org.learning.by.example.reactive.microservices.services;
 
-import org.learning.by.example.reactive.microservices.exceptions.GetLocationException;
+import org.learning.by.example.reactive.microservices.exceptions.GetSunriseSunsetException;
 import org.learning.by.example.reactive.microservices.model.Location;
 import org.learning.by.example.reactive.microservices.model.SunriseSunset;
 import org.learning.by.example.reactive.microservices.model.SunriseSunsetResult;
@@ -8,18 +8,21 @@ import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-public class SunriseSunsetServiceImpl implements SunriseSunsetService{
+public class SunriseSunsetServiceImpl implements SunriseSunsetService {
 
-    private static final String BEGIN_PARAMETERS="?";
-    private static final String NEXT_PARAMETER="&";
-    private static final String EQUALS="=";
-    private static final String LATITUDE_PARAMETER="lat"+EQUALS;
-    private static final String LONGITUDE_PARAMETER="lng"+EQUALS;
+    private static final String BEGIN_PARAMETERS = "?";
+    private static final String NEXT_PARAMETER = "&";
+    private static final String EQUALS = "=";
+    private static final String LATITUDE_PARAMETER = "lat" + EQUALS;
+    private static final String LONGITUDE_PARAMETER = "lng" + EQUALS;
+    private static final String ERROR_GETTING_DATA = "error getting sunrise and sunset";
+    private static final String SUNRISE_RESULT_NOT_OK = "sunrise result was not OK";
+    private static final String STATUS_OK = "OK";
 
     WebClient webClient;
     private final String endPoint;
 
-    public SunriseSunsetServiceImpl(final String endPoint){
+    public SunriseSunsetServiceImpl(final String endPoint) {
         this.endPoint = endPoint;
         this.webClient = WebClient.create();
     }
@@ -29,7 +32,7 @@ public class SunriseSunsetServiceImpl implements SunriseSunsetService{
         return location
                 .transform(this::buildUrl)
                 .transform(this::get)
-                .onErrorResume(throwable -> Mono.error(new GetLocationException("ERROR", throwable)))
+                .onErrorResume(throwable -> Mono.error(new GetSunriseSunsetException(ERROR_GETTING_DATA, throwable)))
                 .transform(this::createResult);
     }
 
@@ -50,9 +53,14 @@ public class SunriseSunsetServiceImpl implements SunriseSunsetService{
                 .flatMap(clientResponse -> clientResponse.bodyToMono(SunriseSunsetResult.class)));
     }
 
-    Mono<SunriseSunset> createResult(final Mono<SunriseSunsetResult> sunriseSunsetResultMono){
-        return sunriseSunsetResultMono.flatMap(sunriseSunsetResult ->
-                Mono.just(new SunriseSunset(sunriseSunsetResult.getResults().getSunrise(),
-                        sunriseSunsetResult.getResults().getSunset())));
+    Mono<SunriseSunset> createResult(final Mono<SunriseSunsetResult> sunriseSunsetResultMono) {
+        return sunriseSunsetResultMono.flatMap(sunriseSunsetResult -> {
+            if ((sunriseSunsetResult.getStatus() != null) && (sunriseSunsetResult.getStatus().equals(STATUS_OK))) {
+                return Mono.just(new SunriseSunset(sunriseSunsetResult.getResults().getSunrise(),
+                        sunriseSunsetResult.getResults().getSunset()));
+            } else {
+                return Mono.error(new GetSunriseSunsetException(SUNRISE_RESULT_NOT_OK));
+            }
+        });
     }
 }
